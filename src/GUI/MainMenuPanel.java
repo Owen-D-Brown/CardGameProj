6213df50c720283a60e3eeddaa8fc872e9285583
Conor <conorfuchs@hotmail.com>
2025-04-06 13:20:12 +0100

    Final Main Menu design implemented

--- src/GUI/MainMenuPanel.java
+++ src/GUI/MainMenuPanel.java
@@ -11,7 +11,7 @@ import java.awt.image.BufferedImage;
 import java.io.InputStream;
 
 public class MainMenuPanel extends JPanel {
-    private static final String IMAGE_PATH = "/Resources/MenuImages/Mainmenu.png";
+    private static final String IMAGE_PATH = "/Resources/MenuImages/MainMenuFinal.png";
     protected BufferedImage image;
     public MainMenuPanel(RootContainer root) {
         //load background image
